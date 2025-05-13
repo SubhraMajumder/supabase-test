11ab54edbc39203ce9fd4548f588883c97cabd4f
SubhraMajumder <subhramajumder32@gmail.com>
2025-05-13 11:36:08 +0530

    Creating Supabase app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import supabase from '../config/supabaseClient'
+import './App.css'
+
+function App() {
+  console.log(supabase);
+  return (
+    <>
+      Hello
+    </>
+  )
+}
+
+export default App
